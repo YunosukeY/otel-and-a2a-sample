e86851e2e1d9848f0236007fbbca5a164a42e244
Yunosuke Yamada <yunosuke.yamada@3-shake.com>
2025-05-05 17:51:27 +0900

    uv add ruff

--- client.py
+++ client.py
@@ -3,16 +3,15 @@ import asyncio
 from uuid import uuid4
 
 client = A2AClient(url="http://localhost:10002")
-res = asyncio.run(client.send_task({
-    "id": str(uuid4()),
-    "message": {
-        "role": "user",
-        "parts": [
-            {
-                "type": "text",
-                "text": "Hello world"
-            }
-        ]
-    },
-}))
+res = asyncio.run(
+    client.send_task(
+        {
+            "id": str(uuid4()),
+            "message": {
+                "role": "user",
+                "parts": [{"type": "text", "text": "Hello world"}],
+            },
+        }
+    )
+)
 print(res.result)
--- server.py
+++ server.py
@@ -9,40 +9,42 @@ from task_manager import MyAgentTaskManager
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
 @click.command()
 @click.option("--host", default="localhost")
 @click.option("--port", default=10002)
 def main(host, port):
-  skill = AgentSkill(
-    id="my-project-echo-skill",
-    name="Echo Tool",
-    description="Echos the input given",
-    tags=["echo", "repeater"],
-    examples=["I will see this echoed back to me"],
-    inputModes=["text"],
-    outputModes=["text"],
-  )
+    skill = AgentSkill(
+        id="my-project-echo-skill",
+        name="Echo Tool",
+        description="Echos the input given",
+        tags=["echo", "repeater"],
+        examples=["I will see this echoed back to me"],
+        inputModes=["text"],
+        outputModes=["text"],
+    )
 
-  capabilities = AgentCapabilities()
-  agent_card = AgentCard(
-    name="Echo Agent",
-    description="This agent echos the input given",
-    url=f"http://{host}:{port}/",
-    version="0.1.0",
-    defaultInputModes=["text"],
-    defaultOutputModes=["text"],
-    capabilities=capabilities,
-    skills=[skill]
-  )
+    capabilities = AgentCapabilities()
+    agent_card = AgentCard(
+        name="Echo Agent",
+        description="This agent echos the input given",
+        url=f"http://{host}:{port}/",
+        version="0.1.0",
+        defaultInputModes=["text"],
+        defaultOutputModes=["text"],
+        capabilities=capabilities,
+        skills=[skill],
+    )
 
-  task_manager = MyAgentTaskManager()
-  server = A2AServer(
-    agent_card=agent_card,
-    task_manager=task_manager,
-    host=host,
-    port=port,
-  )
-  server.start()
+    task_manager = MyAgentTaskManager()
+    server = A2AServer(
+        agent_card=agent_card,
+        task_manager=task_manager,
+        host=host,
+        port=port,
+    )
+    server.start()
+
 
 if __name__ == "__main__":
-  main()
+    main()
